af3b239bf8064451d86915af97206a91bddd7d87
kimdaeyoung <ggaloe95@gmail.com>
2025-05-18 18:29:11 +0900

    feat: 아이콘 이미지 추가

--- src/components/QuickMenu.js
+++ src/components/QuickMenu.js
@@ -1,32 +1,75 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import HoverIcon from "../assets/icons/HoverIcon";
+import searchIcon from "../assets/icons/search.gif";
+import searchIcon2 from "../assets/icons/search.png";
+import recommendIcon from "../assets/icons/recommend.gif";
+import recommendIcon2 from "../assets/icons/recommend.png";
+import rankIcon from "../assets/icons/rank.gif";
+import rankIcon2 from "../assets/icons/rank.png";
+import discussIcon from "../assets/icons/discuss.gif";
+import discussIcon2 from "../assets/icons/discuss.png";
+import listIcon from "../assets/icons/list.gif";
+import listIcon2 from "../assets/icons/list.png";
 
 const menuItems = [
-    { label: "법안 검색", path: "/", icon: "/icons/search.png" },
-    { label: "추천 법안", path: "/recommend", icon: "/icons/recommend.png" },
-    { label: "법안 랭킹", path: "/ranking", icon: "/icons/rank.png" },
-    { label: "법안 토의", path: "/discussion", icon: "/icons/discuss.png" },
-    { label: "법안 리스트", path: "/bills", icon: "/icons/list.png" },
+    {
+        label: "법안 검색",
+        path: "/",
+        staticIcon: searchIcon2,
+        hoverIcon: searchIcon,
+    },
+    {
+        label: "추천 법안",
+        path: "/recommend",
+        staticIcon: recommendIcon2,
+        hoverIcon: recommendIcon,
+    },
+    {
+        label: "법안 랭킹",
+        path: "/ranking",
+        staticIcon: rankIcon2,
+        hoverIcon: rankIcon,
+    },
+    {
+        label: "법안 토의",
+        path: "/discussion",
+        staticIcon: discussIcon2,
+        hoverIcon: discussIcon,
+    },
+    {
+        label: "법안 리스트",
+        path: "/bills",
+        staticIcon: listIcon2,
+        hoverIcon: listIcon,
+    },
 ];
 
 const QuickMenu = () => {
     const navigate = useNavigate();
 
     return (
-        <div style={styles.wrapper}>
-            {menuItems.map((item, idx) => (
-                <div
-                    key={idx}
-                    onClick={() => navigate(item.path)}
-                    style={styles.menuItem}
-                >
-                    <div style={styles.iconWrapper}>
-                        <img src={item.icon} alt={item.label} style={styles.icon} />
+        <>
+            <div style={styles.wrapper}>
+                {menuItems.map((item, idx) => (
+                    <div
+                        key={idx}
+                        onClick={() => navigate(item.path)}
+                        style={styles.menuItem}
+                    >
+                        <HoverIcon
+                            staticSrc={item.staticIcon}
+                            hoverSrc={item.hoverIcon}
+                        />
+                        <div style={styles.label}>{item.label}</div>
                     </div>
-                    <div style={styles.label}>{item.label}</div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+
+            <div style={styles.footer}>
+                Animated icons by Lordicon.com
+            </div>
+        </>
     );
 };
 
@@ -43,27 +86,19 @@ const styles = {
         flexDirection: "column",
         alignItems: "center",
         cursor: "pointer",
-        transition: "transform 0.2s ease",
-    },
-    iconWrapper: {
-        width: 80,
-        height: 80,
-        backgroundColor: "#f5f5f5",
-        borderRadius: 16,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginBottom: 6,
-    },
-    icon: {
-        width: 70,
-        height: 70,
-        objectFit: "contain",
     },
     label: {
         fontSize: "16px",
         color: "#111",
-
+    },
+    footer: {
+        position: "fixed",
+        bottom: 10,
+        right: 20,
+        fontSize: "10px",
+        color: "rgba(0, 0, 0, 0.2)",
+        pointerEvents: "none",
+        zIndex: 1000,
     },
 };
 
